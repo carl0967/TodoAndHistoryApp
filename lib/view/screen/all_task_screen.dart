dce5c49714d72d5c816a639305336c2f775c60ae
carl0967 <yblueycarlo1967@gmail.com>
2023-11-05 17:56:52 +0900

    検索機能を追加

--- lib/view/screen/all_task_screen.dart
+++ lib/view/screen/all_task_screen.dart
@@ -8,69 +8,121 @@ import 'package:src/view/screen/task_detail_screen.dart';
 
 import '../../provider/task_provider.dart';
 
-class AllTaskScreen extends ConsumerWidget {
+class AllTaskScreen extends ConsumerStatefulWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _AllTaskScreenState createState() => _AllTaskScreenState();
+}
+
+class _AllTaskScreenState extends ConsumerState<AllTaskScreen> {
+  String searchQuery = ''; // 検索クエリを保持する変数
+
+  void _filterTasks(String query) {
+    // 検索クエリを更新
+    setState(() {
+      searchQuery = query;
+    });
+    // ここでタスクリストをフィルタリングするロジックを実装します。
+    // 例えば、Providerを使用している場合は、Providerの状態を更新することができます。
+  }
+
+  @override
+  Widget build(BuildContext context) {
     var tasks = ref.watch(taskListProvider);
+    if (searchQuery.isNotEmpty) {
+      tasks = tasks
+          .where((x) => x.name.contains(searchQuery) || x.detail?.contains(searchQuery) == true)
+          .toList();
+    }
 
     return Scaffold(
-      body: ReorderableListView(
-        onReorder: (oldIndex, newIndex) {
-          ref.read(taskListProvider.notifier).reorder(oldIndex, newIndex);
-        },
-        children: tasks
-            .map((task) => ListTile(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TaskDetailScreen(task: task),
-                      ),
-                    );
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    searchQuery = value;
                   },
-                  key: ValueKey(task),
-                  title: task.isHeader
-                      ? Text(task.name)
-                      : Text(
-                          "${DateFormat('MM/dd').format(task.getLastUpdateTime())} ${task.name}"),
-                  subtitle: !task.isHeader && task.getSubTitle() != null
-                      ? Text(task.getSubTitle()!)
-                      : null,
-                  tileColor: task.isHeader ? Colors.grey[200] : null,
-                  enabled: task.isHeader ? false : true,
-                  trailing: !task.isHeader
-                      ? SizedBox(
-                          width: 96,
-                          child: Row(
-                            children: [
-                              IconButton(
-                                  onPressed: () {
-                                    if (task.isVisible) {
-                                      ref
-                                          .read(taskListProvider.notifier)
-                                          .changeVisible(task, false);
-                                    } else {
-                                      ref.read(taskListProvider.notifier).changeVisible(task, true);
-                                    }
-                                  },
-                                  icon: task.isVisible
-                                      ? const Icon(Icons.visibility_off)
-                                      : const Icon(Icons.visibility)),
-                              IconButton(
-                                icon: const Icon(Icons.delete),
-                                onPressed: () async {
-                                  bool? shouldDelete = await _showDeleteConfirmationDialog(context);
-                                  if (shouldDelete == true) {
-                                    ref.read(taskListProvider.notifier).removeTask(task);
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        )
-                      : null,
-                ))
-            .toList(),
+                  decoration: InputDecoration(
+                    labelText: 'Search Task',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // 検索ボタンが押されたときの処理
+                  _filterTasks(searchQuery);
+                },
+              ),
+            ],
+          ),
+          Expanded(
+            child: ReorderableListView(
+              onReorder: (oldIndex, newIndex) {
+                ref.read(taskListProvider.notifier).reorder(oldIndex, newIndex);
+              },
+              children: tasks
+                  .map((task) => ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TaskDetailScreen(task: task),
+                            ),
+                          );
+                        },
+                        key: ValueKey(task),
+                        title: task.isHeader
+                            ? Text(task.name)
+                            : Text(
+                                "${DateFormat('MM/dd').format(task.getLastUpdateTime())} ${task.name}"),
+                        subtitle: !task.isHeader && task.getSubTitle() != null
+                            ? Text(task.getSubTitle()!)
+                            : null,
+                        tileColor: task.isHeader ? Colors.grey[200] : null,
+                        enabled: task.isHeader ? false : true,
+                        trailing: !task.isHeader
+                            ? SizedBox(
+                                width: 96,
+                                child: Row(
+                                  children: [
+                                    IconButton(
+                                        onPressed: () {
+                                          if (task.isVisible) {
+                                            ref
+                                                .read(taskListProvider.notifier)
+                                                .changeVisible(task, false);
+                                          } else {
+                                            ref
+                                                .read(taskListProvider.notifier)
+                                                .changeVisible(task, true);
+                                          }
+                                        },
+                                        icon: task.isVisible
+                                            ? const Icon(Icons.visibility_off)
+                                            : const Icon(Icons.visibility)),
+                                    IconButton(
+                                      icon: const Icon(Icons.delete),
+                                      onPressed: () async {
+                                        bool? shouldDelete =
+                                            await _showDeleteConfirmationDialog(context);
+                                        if (shouldDelete == true) {
+                                          ref.read(taskListProvider.notifier).removeTask(task);
+                                        }
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : null,
+                      ))
+                  .toList(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
